ec9dd954e23c5bc659c48cecce4d1a50c269aa2f
heru97129 <59069753+heru97129@users.noreply.github.com>
2023-09-06 10:28:54 +0200

    anime home

--- components/topspan/Span.js
+++ components/topspan/Span.js
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from './span.module.scss'
+import { motion } from "framer-motion"
+
 export function Span({text}) {
-    return <span className={styles['span']}>
+    return <motion.span className={styles['span']}
+    initial={{ opacity: 0, x: -100 }}
+    animate={{ opacity: 1, x: 0 }}
+    transition={{ delay:.3}}
+    >
         {text}
 
-    </span>;
+    </motion.span>;
 }
