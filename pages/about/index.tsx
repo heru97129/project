91da9704096095543aa5c3e6bfe6b7c76237e448
heru97129 <59069753+heru97129@users.noreply.github.com>
2023-09-07 12:19:17 +0200

    update

---
+++ pages/about/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './about.module.scss'
+
+function index() {
+  return (
+    <div className={styles['index']}>
+        index
+        
+        </div>
+  )
+}
+
+export default index
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 'use client'
+
 import React from 'react'
 import { useState } from 'react'
+import {styles } from  '../../styles/page.css'
 
 export default function Home() {
 
    let [data,setdata] = useState(0)
     return (
-        <main >
+        <main className={styles} >
              {data}
              <button onClick={()=> setdata(data + 1)}>send</button>
 
